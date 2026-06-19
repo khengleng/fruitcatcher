5e6c0db1f1531286bf5ecba39ceba0df06fc120a
khengleng <khenglengt@gmail.com>
2026-06-19 09:11:01 +0700

    fix: allow admin user cors header

--- apps/gateway/server.js
+++ apps/gateway/server.js
@@ -1624,7 +1624,7 @@ app.use((req, res, next) => {
   const allowedAdminOrigin = !origin || ADMIN_ALLOWED_ORIGINS.includes(origin);
   res.setHeader("Access-Control-Allow-Origin", isAdminRequest ? (allowedAdminOrigin ? origin || "null" : "null") : "*");
   res.setHeader("Vary", "Origin");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Admin-Token");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Admin-Token, X-Admin-User");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
   if (req.method === "OPTIONS") {
